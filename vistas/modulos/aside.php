24b74c3540f58c6300b8372295b94371feb37613
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-11-20 12:38:40 -0500

    Barra lateral - Enlazar con sus vistas.
    - Creación de script para cargar contenido.
    - script para el seleccionado aparezca azul.

--- vistas/modulos/aside.php
+++ vistas/modulos/aside.php
@@ -2,19 +2,20 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="vistas/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
+        <img src="vistas/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Mi Market POS</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        
+
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a style="cursor: pointer;" class="nav-link active"
+                        onclick="CargarContenido('vistas/dashboard.php','content-wrapper')">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Tablero Principal
@@ -22,7 +23,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link active">
+                    <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Productos
@@ -31,19 +32,25 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a style="cursor: pointer;" class="nav-link"
+                                onclick="CargarContenido('vistas/productos.php','content-wrapper')">
+
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Inventario</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a style="cursor: pointer;" class="nav-link"
+                                onclick="CargarContenido('vistas/carga_masiva_productos.php','content-wrapper')">
+
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Carga Masiva</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a style="cursor: pointer;" class="nav-link"
+                                onclick="CargarContenido('vistas/categorias.php','content-wrapper')">
+
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Categorías</p>
                             </a>
@@ -51,31 +58,39 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a style="cursor: pointer;" class="nav-link"
+                        onclick="CargarContenido('vistas/ventas.php','content-wrapper')">
+
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Ventas
                         </p>
                     </a>
                 </li>
-                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                <li class="nav-item">
+                    <a style="cursor: pointer;" class="nav-link"
+                        onclick="CargarContenido('vistas/compras.php','content-wrapper')">
+
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Compras
                         </p>
                     </a>
                 </li>
-                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                <li class="nav-item">
+                    <a style="cursor: pointer;" class="nav-link"
+                        onclick="CargarContenido('vistas/reportes.php','content-wrapper')">
+
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Reportes
                         </p>
                     </a>
                 </li>
-                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                <li class="nav-item">
+                    <a style="cursor: pointer;" class="nav-link"
+                        onclick="CargarContenido('vistas/configuracion.php','content-wrapper')">
+
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Configuración
@@ -88,3 +103,10 @@
     </div>
     <!-- /.sidebar -->
 </aside>
+
+<script>
+    $(".nav-link").on('click',function() {
+       $(".nav-link").removeClass('active');
+       $(this).addClass('active'); 
+    })
+</script>
