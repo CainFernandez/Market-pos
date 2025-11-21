7d04692042ad0f32eab257a02f1c4fe044141028
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-11-21 14:15:12 -0500

    Vista para panel de control.

--- vistas/dashboard.php
+++ vistas/dashboard.php
@@ -20,6 +20,111 @@
 <div class="content">
     <div class="container-fluid">
 
-    </div><!-- /.container-fluid -->
+        <!-- Creamos una fila -->
+        <div class="row">
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h4 id="totalProductos">125</h4>
+
+                        <p>Productos registrados</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="cursor:pointer;" class="small-box-footer">More info
+                        <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <!-- TARJETA TOTAL COMPRAS-->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h4 id="totalCompras">B/. 2,500.00</h4>
+
+                        <p>Total Compras</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="cursor:pointer;" class="small-box-footer">More info
+                        <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <!-- TARJETA TOTAL VENTAS -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h4 id="totalVentas">B/. 1,200.00</h4>
+
+                        <p>Total Ventas</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="cursor:pointer;" class="small-box-footer">More info
+                        <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+
+            <!-- TARJETA TOTAL GANANCIAS -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h4 id="totalGanacias">B/. 470.00</h4>
+
+                        <p>Total Ganancias</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="cursor:pointer;" class="small-box-footer">More info 
+                        <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+
+            <!-- TARJETA MINIMO STOCK -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-primary">
+                    <div class="inner">
+                        <h4 id="totalProductosMinStock">15</h4>
+
+                        <p>Productos pocos stock</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="cursor:pointer;" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <!-- TARJETA TOTAL VENTA DIA ACTUAL -->
+            <div class="col-lg-2">
+                <!-- small box -->
+                <div class="small-box bg-secondary">
+                    <div class="inner">
+                        <h4 id="totalVentasHoy">B/. 250.00</h4>
+
+                        <p>Ventas del dìa</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-clipboard"></i>
+                    </div>
+                    <a style="cursor:pointer;" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </div>
 </div>
 <!-- /.content -->
