037329c303a323cfcf9b8a074633fb1d93c3773f
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-11-22 15:58:49 -0500

    Ventas mes actual - Solicitud de accion y controlador

--- ajax/dashboard.ajax.php
+++ ajax/dashboard.ajax.php
@@ -2,16 +2,33 @@
 
 require_once "../controladores/dashboard.controlador.php";
 require_once "../modelos/dashboard.modelo.php";
-class AjaxDashboard{
-    public function getDatosDashboard(){
+class AjaxDashboard
+{
+    public function getDatosDashboard()
+    {
 
         $datos = DashboardControlador::ctrGetDatosDashboard();
 
         echo json_encode($datos);
     }
+
+    public function getVentasMesActual()
+    {
+        $ventasMesActual = DashboardControlador::ctrGetventasMesActual();
+        echo json_encode($ventasMesActual);
+    }
 }
 
-$datos = new AjaxDashboard();
-$datos -> getDatosDashboard();
+if (isset($_POST["accion"]) && $_POST['accion'] == 1) {
+
+    $ventasMesActual = new AjaxDashboard();
+    $ventasMesActual -> getVentasMesActual();
+
+} else {
+
+    $datos = new AjaxDashboard();
+    $datos->getDatosDashboard();
+
+}
 
 ?>
